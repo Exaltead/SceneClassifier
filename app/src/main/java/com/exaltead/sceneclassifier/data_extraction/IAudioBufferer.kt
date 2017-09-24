178b2ed90d3418d0d1073b7ab25fa6a8a475553c
aku <aku_pohjola@hotmail.fi>
2017-09-24 14:03:22 +0300

    Added mock implementation to audio recorder

--- app/src/main/java/com/exaltead/sceneclassifier/data_extraction/IAudioBufferer.kt
+++ app/src/main/java/com/exaltead/sceneclassifier/data_extraction/IAudioBufferer.kt
@@ -2,13 +2,5 @@ package com.exaltead.sceneclassifier.data_extraction
 
 interface IAudioBufferer {
 
-    /// Starts feeding audio to the ringbuffer
-    fun start()
-
-    /// Stops the audio feeding
-    fun stop()
-
-    /// If the system is feeding data, try to call the callback at most every elapsed second.
-    /// Time between each call might take longer than the minimum time.
-    fun subscribeToElapsedTime(callback:(List<Double>)-> Unit, minimumTime: Double = 0.5)
+    fun takeShortAudioRecord(duration: Float): ShortArray
 }
