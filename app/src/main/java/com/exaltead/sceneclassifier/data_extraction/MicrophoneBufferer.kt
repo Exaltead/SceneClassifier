733e20ec446ea370ad86173f1159cbb5a9cb4b68
aku <aku_pohjola@hotmail.fi>
2017-09-24 14:11:33 +0300

    Added micropohne bufferer

---
+++ app/src/main/java/com/exaltead/sceneclassifier/data_extraction/MicrophoneBufferer.kt
@@ -0,0 +1,28 @@
+package com.exaltead.sceneclassifier.data_extraction
+
+import android.media.AudioFormat.*
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import android.util.Log
+
+const val samplingRate = 20000 // not used, use the default currently
+
+class MicrophoneBufferer : IAudioBufferer {
+
+    private val audioRecorder: AudioRecord
+    init {
+        val minBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE_UNSPECIFIED, CHANNEL_IN_MONO,
+                ENCODING_PCM_FLOAT)
+        Log.i("AudioRecorder", "min buffer size "+ minBufferSize.toString())
+        // Use bigger buffer if needed
+        audioRecorder = AudioRecord(MediaRecorder.AudioSource.MIC, samplingRate,
+                CHANNEL_IN_MONO, ENCODING_PCM_8BIT, minBufferSize)
+    }
+    override fun takeShortAudioRecord(duration: Float): ShortArray {
+        //TODO: Implement
+        val samplinRate = audioRecorder.sampleRate
+        val requiredArrayLength = duration * samplinRate
+        return kotlin.ShortArray(requiredArrayLength.toInt() + 1)
+    }
+
+}
