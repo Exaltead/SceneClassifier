1440b163ff586aa32d3333183039c17984ccd60b
aku <aku_pohjola@hotmail.fi>
2017-09-24 14:11:10 +0300

    Added mock implementation to WawReader

--- app/src/main/java/com/exaltead/sceneclassifier/data_extraction/WawBuffererI.kt
+++ app/src/main/java/com/exaltead/sceneclassifier/data_extraction/WawBuffererI.kt
@@ -1,15 +1,8 @@
 package com.exaltead.sceneclassifier.data_extraction
 
-class WawBuffererI : IAudioBufferer {
-    override fun start() {
-        TODO("not implemented")
+class WawBufferer : IAudioBufferer {
+    override fun takeShortAudioRecord(duration: Float): ShortArray {
+        return kotlin.ShortArray(0)
     }
 
-    override fun stop() {
-        TODO("not implemented")
-    }
-
-    override fun subscribeToElapsedTime(callback: () -> Unit, minimumTime: Double) {
-        TODO("not implemented")
-    }
 }
