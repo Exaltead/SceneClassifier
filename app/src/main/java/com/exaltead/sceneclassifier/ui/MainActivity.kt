6f63b3915b1566d5fbd704dcae2f4839a7e38d8e
Aku <aku.pohjola@student.tut.fi>
2017-09-29 14:55:46 +0300

    Added permission requesting.

--- app/src/main/java/com/exaltead/sceneclassifier/ui/MainActivity.kt
+++ app/src/main/java/com/exaltead/sceneclassifier/ui/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.exaltead.sceneclassifier.ui
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import com.exaltead.sceneclassifier.R
 import com.exaltead.sceneclassifier.classification.ClassifiationService
 import com.exaltead.sceneclassifier.classification.ClassifierServiceConncetion
 import kotlinx.android.synthetic.main.activity_main.*
+private const val TAG = "MainActivity"
+private const val RECORD_AUDIO_CODE = 300
 
 class MainActivity : Activity() {
-    private val connection: ClassifierServiceConncetion = ClassifierServiceConncetion()
 
+    private val connection: ClassifierServiceConncetion = ClassifierServiceConncetion()
+    private var hasPermissionToRecordAudio = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +27,7 @@ class MainActivity : Activity() {
 
     override fun onStart() {
         super.onStart()
+        checkAndRequestPermission()
         val intent = Intent(this, ClassifiationService::class.java)
         bindService(intent, connection, Context.BIND_AUTO_CREATE)
     }
@@ -28,5 +37,32 @@ class MainActivity : Activity() {
         if(connection.binder != null){
             unbindService(connection)
         }
-}
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray?) {
+        if(requestCode == RECORD_AUDIO_CODE && grantResults?.size == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Log.i(TAG, "Permission granted")
+            hasPermissionToRecordAudio = true
+        }
+        else{
+            Log.w(TAG, "Permission not granted")
+        }
+    }
+
+    private fun checkAndRequestPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED){
+            Log.i(TAG, "Permission is not yet granted")
+
+            // TODO: show rationale: https://developer.android.com/training/permissions/requesting.html
+
+            ActivityCompat.requestPermissions(this, Array(1,
+                    { _ -> Manifest.permission.RECORD_AUDIO}), RECORD_AUDIO_CODE)
+        }
+        else{
+            Log.i(TAG, "Permission already granted")
+            hasPermissionToRecordAudio = true
+        }
+    }
 }
