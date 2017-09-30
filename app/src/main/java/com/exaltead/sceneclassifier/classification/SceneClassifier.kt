67922f44b3234e7c83e526ee72bd52f9708d167b
Aku <aku.pohjola@student.tut.fi>
2017-09-30 17:33:15 +0300

    Removed debug logging.

--- app/src/main/java/com/exaltead/sceneclassifier/classification/SceneClassifier.kt
+++ app/src/main/java/com/exaltead/sceneclassifier/classification/SceneClassifier.kt
@@ -1,14 +1,12 @@
 package com.exaltead.sceneclassifier.classification
 
-import android.util.Log
 import com.exaltead.sceneclassifier.data_extraction.IFeatureExtractor
+
 private const val TAG = "SceneClassifier"
 
 class SceneClassifier(private val featureExtractor: IFeatureExtractor) {
     fun getCurrentClassification(): List<ClassificationResult> {
         val samples = featureExtractor.receiveFeaturesForTimeSpan(1.0)
-        Log.d("SceneClassifer", "Pulled "+ samples.size.toString() + " features")
-
         //"Classification" results
         return groupByTime(samples).mapIndexed({ i, d -> ClassificationResult(i.toString(), d)})
     }
@@ -20,7 +18,6 @@ private fun groupByTime(samples:Array<Float>): List<Double>{
     val accumulator: MutableList<Float> = mutableListOf()
     for ( i in samples.indices){
         if(i % samples.size / 10 == 0 && i != 0){
-            Log.i(TAG, "Smallest "+ accumulator.min().toString()+ "Largest " + accumulator.max())
             result.add(accumulator.average())
             accumulator.clear()
         }
