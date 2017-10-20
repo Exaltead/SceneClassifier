df8ce71618ef12ddd9ab551c65e0181d3517ad73
aku <aku_pohjola@hotmail.fi>
2017-10-20 21:17:44 +0300

    Added base for waw file handling.

--- DataExtraction/src/main/kotlin/WawIterator.kt
+++ DataExtraction/src/main/kotlin/WawIterator.kt
@@ -1,29 +1,29 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Path
 import javax.sound.sampled.AudioInputStream
 
 
 private data class DatasetHolder(val path: String, val type: String, val location: String)
 
-class WawIterable: Iterable<FloatArray> {
-    private val locations: MutableList<DatasetHolder> = mutableListOf()
-
+class WawIterable(metaFileLocation: String) : Iterable<FloatArray> {
+    private val locations: List<DatasetHolder> = readMetadataFile(metaFileLocation)
     override fun iterator(): Iterator<FloatArray> {
         return WawIterator(this)
     }
 
-    init {
-        //TODO: Implement reading of the wav files
-    }
-
-    fun canReadNext(): Boolean{
+    fun canReadNext(): Boolean {
         return false
     }
 
-    fun getNext(): FloatArray{
+    fun getNext(): FloatArray {
         return FloatArray(0)
     }
 }
 
-private class WawIterator(private val iterable: WawIterable): Iterator<FloatArray>{
+private class WawIterator(private val iterable: WawIterable) : Iterator<FloatArray> {
     override fun hasNext(): Boolean {
         return iterable.canReadNext()
     }
@@ -34,7 +34,19 @@ private class WawIterator(private val iterable: WawIterable): Iterator<FloatArra
 
 }
 
-private fun AudioInputStream.toFloatArray():FloatArray{
+private fun AudioInputStream.toFloatArray(): FloatArray {
     //TODO: implement
     TODO()
+}
+
+private fun String.parseDatasetHolder(): DatasetHolder {
+    return this.trim().split('\t').toDatasetHolder()
+}
+
+private fun List<String>.toDatasetHolder(): DatasetHolder {
+    return DatasetHolder(this[0], this[1], this[2])
+}
+
+private fun readMetadataFile(filename: String): List<DatasetHolder> {
+    return BufferedReader(FileReader(filename)).useLines {  t -> t.map { l -> l.parseDatasetHolder() }.toList() }
 }
