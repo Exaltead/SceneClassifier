d8185dc98c7380cb1669f532a0f4893822970b1f
aku <aku_pohjola@hotmail.fi>
2017-11-07 13:29:47 +0200

    Added basis for model

--- SceneClassifier/app/src/main/java/com/exaltead/sceneclassifier/classification/SceneClassifier.kt
+++ SceneClassifier/app/src/main/java/com/exaltead/sceneclassifier/classification/SceneClassifier.kt
@@ -1,12 +1,36 @@
 package com.exaltead.sceneclassifier.classification
 
+import android.os.Environment
 import com.exaltead.sceneclassifier.data_extraction.IFeatureExtractor
+import org.tensorflow.Graph
+import org.tensorflow.Session
+import org.tensorflow.Tensor
+import java.nio.file.Files
+import java.nio.file.Path
 
 private const val TAG = "SceneClassifier"
+private const val MODEL_FILE_NAME = "/model.pb"
 
 class SceneClassifier(private val featureExtractor: IFeatureExtractor) {
+    val labels = List(15, {i -> i.toString()})
+    private val sess: Session
+    init {
+        // Read the target file
+
+        val protoBytes = readBytesFromProtobuf()
+        val graph = Graph()
+        graph.importGraphDef(protoBytes)
+
+        sess = Session(graph)
+        // init a classifier for it
+        // do magic
+    }
+
     fun getCurrentClassification(): List<ClassificationResult> {
         val samples = featureExtractor.receiveFeaturesForTimeSpan()
+        // help from https://github.com/tensorflow/tensorflow/blob/master/tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java
+        //val result: Tensor<Float> = sess.runner().fetch("")
+
         //"Classification" results
         return groupByTime(samples).mapIndexed({ i, d -> ClassificationResult(i.toString(), d)})
     }
@@ -25,4 +49,8 @@ private fun groupByTime(samples:Array<Float>): List<Double>{
     }
     result.add(accumulator.average())
     return result
+}
+
+private fun readBytesFromProtobuf(filename: String): ByteArray{
+
 }
