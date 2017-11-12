68edfc0fd54a1f64fefc61c52d6b285fecfb3c4a
aku <aku_pohjola@hotmail.fi>
2017-11-12 07:35:35 +0200

    Base for online usage of tensorflow in made

--- SceneClassifier/app/src/main/java/com/exaltead/sceneclassifier/classification/SceneClassifier.kt
+++ SceneClassifier/app/src/main/java/com/exaltead/sceneclassifier/classification/SceneClassifier.kt
@@ -1,39 +1,59 @@
 package com.exaltead.sceneclassifier.classification
 
 import android.os.Environment
+import android.util.Log
 import com.exaltead.sceneclassifier.data_extraction.IFeatureExtractor
 import org.tensorflow.Graph
 import org.tensorflow.Session
 import org.tensorflow.Tensor
-import java.nio.file.Files
-import java.nio.file.Path
+import java.io.FileInputStream
+import java.nio.FloatBuffer
+
 
 private const val TAG = "SceneClassifier"
-private const val MODEL_FILE_NAME = "/model.pb"
+private val MODEL_FILE_NAME = Environment.getExternalStorageDirectory().name+"/model.pb"
+private const val OUTPUT_TENSOR_NAME = "tuutuut"
+private const val INPUT_TENSOR_NAME = "dadaa"
+private const val SAMPLE_LENGHT = 30L
+private const val NUMBER_OF_CLASSES = 15
 
 class SceneClassifier(private val featureExtractor: IFeatureExtractor) {
     val labels = List(15, {i -> i.toString()})
-    private val sess: Session
+    private val graph = Graph()
     init {
         // Read the target file
 
-        val protoBytes = readBytesFromProtobuf()
-        val graph = Graph()
+        val protoBytes = readBytesFromProtobuf(MODEL_FILE_NAME)
         graph.importGraphDef(protoBytes)
+        Log.d(TAG, "Loaded model")
 
-        sess = Session(graph)
+
         // init a classifier for it
         // do magic
     }
 
     fun getCurrentClassification(): List<ClassificationResult> {
         val samples = featureExtractor.receiveFeaturesForTimeSpan()
-        // help from https://github.com/tensorflow/tensorflow/blob/master/tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java
-        //val result: Tensor<Float> = sess.runner().fetch("")
-
+        // help from https://github.com/tensorflow/tensorflow/blob/master
+        // /tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java
+        val res = Session(graph).use { classify(it, samples.toFloatArray()) }
         //"Classification" results
-        return groupByTime(samples).mapIndexed({ i, d -> ClassificationResult(i.toString(), d)})
+        //return groupByTime(samples).mapIndexed({ i, d -> ClassificationResult(i.toString(), d)})
+        return res.mapIndexed({ i, d -> ClassificationResult(i.toString(), d.toDouble())});
     }
+
+}
+private fun classify(sess: Session, samples: FloatArray): FloatArray{
+    val buffer = FloatBuffer.wrap(FloatArray(NUMBER_OF_CLASSES))
+    Tensor.create(longArrayOf(SAMPLE_LENGHT), FloatBuffer.wrap(samples))
+            .use { o -> runTensors(sess, o, buffer)}
+    return buffer.array()
+}
+
+private fun runTensors(sess: Session, inputTensor: Tensor<Float>, buffer: FloatBuffer) {
+    sess.runner().feed(INPUT_TENSOR_NAME, inputTensor)
+            .fetch(OUTPUT_TENSOR_NAME).run()
+            .map { t -> t.use { it.writeTo(buffer) } }
 
 }
 
@@ -52,5 +72,7 @@ private fun groupByTime(samples:Array<Float>): List<Double>{
 }
 
 private fun readBytesFromProtobuf(filename: String): ByteArray{
-
+    return FileInputStream(filename).use {
+        it.readBytes()
+    }
 }
